1218f3b829bb29ac5fd889095066e9aeb6df9467
autogamer-daveloper <158582491+autogamer-daveloper@users.noreply.github.com>
2026-09-21 22:41:18 +0500

    Updated menu (0.0.4.2)

--- Scripts/MainMenu/VisualAccessoriesShop.cs
+++ Scripts/MainMenu/VisualAccessoriesShop.cs
@@ -6,28 +6,48 @@ using TMPro;
 public class VisualAccessoryProduct
 {
     [Header(" Main settings ")]
+    [Tooltip("Accessory id. Need to be unique. Counting from 1.")]
     public int visualId = 0; // counting from 1
+    [Tooltip("Accessory key. Need to be unique. May use key like 'visual_bought_(visualId)'.")]
     public string boughtKey = "visual_bought_0";
+    [Tooltip("Button, on click which you will select this accessory.")]
     public Button use;
-    public GameObject usingDot;
+    /*[Tooltip("Temporary! Shows what accessory was selected.")]
+    public GameObject usingDot; */
+    [Tooltip("Shows what accessory was selected.")]
+    public Image ramkImage;
     [Space(10)]
     [Header(" Shop settings, if can be bought ")]
+    [Tooltip("Is this accessory can be bought?")]
     public bool canBuy = true;
+    [Tooltip("If you can buy it, which it cost for player?")]
     public int price = 1200;
+    [Tooltip("If you can buy it, where price will shown?")]
     public TMP_Text priceText;
+    [Tooltip("If you can buy, with which button player will buy this accessory&")]
     public Button buy;
+    [Tooltip("Same object that was in field 'buy'.")]
     public GameObject buyButton;
 }
 
 public class VisualAccessoriesShop : MonoBehaviour
 {
     [Header("__ Products __")]
+    [Tooltip("Set up your accessories.")]
     [SerializeField] private VisualAccessoryProduct[] products;
     [Header("__ Extra UI __")]
+    [Tooltip("Button, on click which remove all accessories.")]
     [SerializeField] private Button removeVisualAccessories;
+    [Tooltip("Astraslimes (vallet) text. Show value of this vallet.")]
     [SerializeField] private TMP_Text astraslimesText;
+    [Tooltip("Help tip object.")]
     [SerializeField] private GameObject helpNotEnought;
+    [Tooltip("Help tip animation component.")]
     [SerializeField] private Animation helpNotEnoughtAnim;
+    [Tooltip("In active ramk sprite.")]
+    [SerializeField] private Sprite inactiveSlot;
+    [Tooltip("Active ramk sprite.")]
+    [SerializeField] private Sprite activeSlot;
 
     private int _astraslimes = 0;
 
@@ -44,10 +64,10 @@ public class VisualAccessoriesShop : MonoBehaviour
         {
             int index = i;
             int isBought = KeyManager.Get_Bool_Key(products[index].boughtKey);
-            if (products[index].buyButton != null)
+            if (products[index].buy != null)
             {
-                if (isBought != 0) { products[index].buyButton.SetActive(false); }
-                else { products[index].buyButton.SetActive(true); }
+                if (isBought != 0) { products[index].buy.gameObject.SetActive(false); }
+                else { products[index].buy.gameObject.SetActive(true); }
             }
             if (products[index].canBuy)
             {
@@ -56,8 +76,8 @@ public class VisualAccessoriesShop : MonoBehaviour
                 products[index].buy.onClick.AddListener(() => { BuyVisualAccessory(index); });
             }
             int isUsing = KeyManager.GetInt_VisualAccessoryID();
-            if (isUsing == products[index].visualId) { products[index].usingDot.SetActive(true); }
-            else { products[index].usingDot.SetActive(false); }
+            if (isUsing == products[index].visualId) { /*products[index].usingDot.SetActive(true);*/ products[index].ramkImage.sprite = activeSlot; }
+            else { /*products[index].usingDot.SetActive(false);*/ products[index].ramkImage.sprite = inactiveSlot; }
             products[index].use.onClick.RemoveAllListeners();
             products[index].use.onClick.AddListener(() => { UseVisualAccessory(index); });
             if (isBought != 0) { products[index].use.interactable = true; }
